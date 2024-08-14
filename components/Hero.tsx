1f4d5303e34c90edfed724010deed3b84a953281
Rohit Nayak <rohitn822@gmail.com>
2024-08-14 20:45:14 +0530

    dashboard

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar' 
 import Dashboard from './Dashboard/Dashboard'
+import CardList from './CardList/CardList'
 
 const Hero = () => {
   return (
     <div>
       <Navbar />
       <Dashboard />
+      <CardList />
     </div>
   )
 }
